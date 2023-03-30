d7d3e0b084999606ab8e3d19bf9f2847b62ca368
vozaldi <vozaldi@gmail.com>
2023-03-30 13:30:25 +0700

    fix: Remove unused components

--- src/pages/PageA.tsx
+++ src/pages/PageA.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Button, View, Text, StyleSheet, RefreshControl } from "react-native";
 import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 import { useNavigation } from "@react-navigation/core";
@@ -12,49 +11,43 @@ function PageA() {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={[styles.sectionContainer, { flexGrow: 1, width: '100%' }]}>
-        <Animated.ScrollView
-          // Comment this props `refreshControl` to prevent crashing on `navigate.goBack()`
-          refreshControl={(
-            <RefreshControl
-              refreshing={isLoading}
-              onRefresh={() => void(0)}
-              colors={['orange']}
-            />
-          )}
-          style={[{ flex: 1, width: '100%' }]}
-          contentContainerStyle={[{ alignItems: 'center', justifyContent: 'center', width: '100%' }]}
+    <View style={[styles.sectionContainer, { flexGrow: 1, width: '100%' }]}>
+      <Animated.ScrollView
+        // Comment this props `refreshControl` to prevent crashing on `navigate.goBack()`
+        refreshControl={(
+          <RefreshControl refreshing={isLoading} />
+        )}
+        style={[{ flex: 1, width: '100%' }]}
+        contentContainerStyle={[{ alignItems: 'center', justifyContent: 'center', width: '100%' }]}
+      >
+        <Animated.View
+          key={`zoom-2`}
+          entering={ZoomIn}
+          // Comment this prop `exiting` to prevent crashing
+          exiting={ZoomOut}
         >
-          <Animated.View
-            key={`zoom-2`}
-            entering={ZoomIn}
-            // Comment this prop `exiting` to prevent crashing
-            exiting={ZoomOut}
-          >
-            <Text>Zoom In and Out</Text>
-          </Animated.View>
+          <Text>Zoom In and Out</Text>
+        </Animated.View>
 
-          {Array.from(Array(25)).map((item, index) => (
-            <Text key={index} style={{ paddingVertical: 8 }}>
-              Text {index}
-            </Text>
-          ))}
-        </Animated.ScrollView>
-
-        <View style={{ marginTop: 24, alignSelf: 'center' }}>
-          <Button
-            title='Check Crash'
-            color="orange"
-            onPress={() => navigation.goBack()}
-          />
-
-          <Text style={{ marginTop: 12 }}>
-            or press back button to Check Crash
+        {Array.from(Array(25)).map((item, index) => (
+          <Text key={index} style={{ paddingVertical: 8 }}>
+            Text {index}
           </Text>
-        </View>
+        ))}
+      </Animated.ScrollView>
+
+      <View style={{ marginTop: 24, alignSelf: 'center' }}>
+        <Button
+          title='Check Crash'
+          color="orange"
+          onPress={() => navigation.goBack()}
+        />
+
+        <Text style={{ marginTop: 12 }}>
+          or press back button to Check Crash
+        </Text>
       </View>
-    </SafeAreaView>
+    </View>
   )
 }
 
